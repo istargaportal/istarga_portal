84622e0843db050aef372d363ee558c6ff11a610
maheshmthroat <maheshmthorat@gmail.com>
2020-08-31 10:39:30 +0530

    'updated'

--- system-client/API/Action-Bulk-Order.php
+++ system-client/API/Action-Bulk-Order.php
@@ -70,7 +70,7 @@ if(@$load_condition == "import_bulk_order")
 	}
 	
 	$inputFileName = 'import_bulk_order.xlsx'; 
-	
+
 	try {
 		$objPHPExcel = PHPExcel_IOFactory::load($inputFileName);
 	} catch(Exception $e) {
@@ -83,9 +83,6 @@ if(@$load_condition == "import_bulk_order")
 	$error_code = 0; $alredy_exists = "";
 	$from_date_time = $from_date.' '.$from_time;
 	$to_date_time = $to_date.' '.$to_time;
-
-	echo "MAHESH THROAT";
-	exit();
 
 	$check_2 = "SELECT Inv_Code FROM Client WHERE id = '".$client_id."' ";
     $resul_2 = mysqli_query($db,$check_2); 
